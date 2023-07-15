9940dbd65a4c8053e0c3093ace3916b49dd61e61
seokho10007 <sukho1007@naver.com>
2023-07-15 20:52:30 +0900

    feat: ConfigModule 환경변수 설정 및 Module 주입 (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AppController } from "@/app.controller";
+import { AppService } from "@/app.service";
+import { CommonModule } from "@/common/modules";
 
 @Module({
-  imports: [],
+  imports: [CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
