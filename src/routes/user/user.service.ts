9f6ef8af93cf55fc83b33b823b2ec3ff908808e2
seokho10007 <sukho1007@naver.com>
2023-07-23 20:01:17 +0900

    feat: user service 비즈니스 로직 생성 (#8)

--- src/routes/user/user.service.ts
+++ src/routes/user/user.service.ts
@@ -1,24 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 
+import { CreateUserDTO } from "@/routes/user/dto/create-user.dto";
 import { UserRepository } from "@/routes/user/user.repository";
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async create(input: any) {
-    return;
+  async create(createUserDto: CreateUserDTO) {
+    const user = await this.userRepository.getByUserId(createUserDto.userId);
+
+    if (user) throw new ConflictException("이미 존재하는 유저입니다.");
+
+    return this.userRepository.create(createUserDto);
   }
 
   async getByUserId(userId: string) {
-    return;
+    return this.userRepository.getByUserId(userId);
   }
 
-  async getById(id: string) {
-    return;
+  async getById(_id: string) {
+    return this.userRepository.getById(_id);
   }
 
-  async updateRefreshToken(id: string, refreshToken: string = null) {
-    return;
+  async updateRefreshToken(_id: string, refreshToken: string = null) {
+    return this.userRepository.updateRefreshToken(_id, refreshToken);
   }
 }
