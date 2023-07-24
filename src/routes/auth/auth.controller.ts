5b441253f35bd0e40e4c5d8ef3b51bf4760e5eca
seokho10007 <sukho1007@naver.com>
2023-07-24 23:56:11 +0900

    feat: auth controller 비지니스 로직 구현 (#7)

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Response } from "express";
+
+import { Controller, Post, Res } from "@nestjs/common";
 
 import { AuthService } from "@/routes/auth/auth.service";
+import { SigninDTO } from "@/routes/auth/dto/signin.dto";
 import { UserService } from "@/routes/user/user.service";
+import { EJwtTokenType, TTokenUser } from "@/types";
 
 @Controller("auth")
 export class AuthController {
@@ -10,19 +14,52 @@ export class AuthController {
   // localGuard를 통해 검증된 User 정보를 가져옴
   // 해당 User 정보를 통해 access token과 refresh token을 발급
   // 발급된 token을 response header에 담아서 전송
-  async signin(SigninDTO: any, user: any, res: any) {
-    return ["string", "string"];
+  @Post("signin")
+  async signin(_: SigninDTO, _user: TTokenUser, @Res({ passthrough: true }) res: Response) {
+    const user = await this.userService.getById(_user._id);
+
+    const [accessToken, refreshToken] = await this.authService.signin(user);
+
+    this.authService.registerTokenInCookie({
+      type: EJwtTokenType.ACCESS,
+      token: accessToken,
+      res,
+    });
+
+    this.authService.registerTokenInCookie({
+      type: EJwtTokenType.REFRESH,
+      token: refreshToken,
+      res,
+    });
+
+    return { username: user.username };
   }
 
   // AuthGuard를 통해 검증된 User 정보를 가져옴
   // response header에 담겨있는 token을 제거
-  async signout(user: any, res: any) {
-    return;
+  @Post("signout")
+  async signout(_user: TTokenUser, @Res({ passthrough: true }) res: Response) {
+    await this.userService.updateRefreshToken(_user._id, null);
+
+    this.authService.clearCookie(res);
+
+    return true;
   }
 
   // AuthGuard를 통해 검증된 User 정보를 가져옴
   // 검증된 User 정보를 통해 access token 재발급
-  async refresh(user: any, res: any) {
-    return;
+  @Post("refresh")
+  async refresh(_user: TTokenUser, @Res({ passthrough: true }) res: Response) {
+    await this.authService.verifyRefreshToken(_user._id);
+
+    const [accessToken] = await this.authService.signin(_user);
+
+    this.authService.registerTokenInCookie({
+      type: EJwtTokenType.ACCESS,
+      token: accessToken,
+      res,
+    });
+
+    return true;
   }
 }
