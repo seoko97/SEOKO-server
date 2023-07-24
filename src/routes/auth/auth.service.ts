defc40608e3f24d79cc0d2cbb54b34b5ebe0f7b1
seokho10007 <sukho1007@naver.com>
2023-07-24 23:12:22 +0900

    feat: auth service refresh token 유효성 검사 (#7)

--- src/routes/auth/auth.service.ts
+++ src/routes/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService, JwtSignOptions } from "@nestjs/jwt";
+
+import * as bcrypt from "bcrypt";
 
 import { AuthConstantProvider } from "@/common/providers/auth-constant.provider";
 import { TTokenUser } from "@/types";
+import { USER_ERROR } from "@/utils/constants";
 
 @Injectable()
 export class AuthService {
@@ -26,16 +29,16 @@ export class AuthService {
 
   // refresh token 유효성 검사
   // 서버에 저장된 유저 토큰이 유효한 토큰인지 확인
-  async verifyRefreshToken(user: any, refreshToken: string) {
-    return true;
-  }
+  async verifyRefreshToken(refreshToken: string) {
+    try {
+      const isVerify = await this.jwtService.verify(refreshToken);
 
-  // 로그인 요청시 입력한 아이디와 비밀번호가 유저 정보와 일치하는지 확인
-  // 확인됐다면 유저 정보를 반환
-  // 유저 패스워드와 입력받은 패스워드를 비교
-  // 함수 단위를 작게 가져가기위해 내부에서 user 정보를 가져오지 않음
-  async comparePassword(uPassword: string, password: string) {
-    return true;
+      if (!Boolean(isVerify)) throw new UnauthorizedException();
+
+      return true;
+    } catch (e) {
+      throw new UnauthorizedException(USER_ERROR.UNAUTHORIZED);
+    }
   }
 
   // 입력받은 데이터를 통해 토큰을 발급
--- test/routes/auth/auth.service.spec.ts
+++ test/routes/auth/auth.service.spec.ts
@@ -1,9 +1,8 @@
 import { JwtModule, JwtService } from "@nestjs/jwt";
 import { TestingModule } from "@nestjs/testing";
 
-import * as bcrypt from "bcrypt";
 import createTestingModule from "test/utils/mongo/createTestModule";
-import { TOKEN_STUB, TOKEN_USER_STUB, USER_STUB, USER_STUB_NON_PASSWORD } from "test/utils/stub";
+import { TOKEN_STUB, TOKEN_USER_STUB, USER_STUB } from "test/utils/stub";
 
 import { AuthConstantProvider } from "@/common/providers/auth-constant.provider";
 import { AuthService } from "@/routes/auth/auth.service";
@@ -11,7 +10,6 @@ import { USER_ERROR } from "@/utils/constants";
 
 jest.mock("@/routes/user/user.service");
 jest.mock("@/common/providers/auth-constant.provider");
-jest.mock("bcrypt");
 
 describe("AuthService", () => {
   let authService: AuthService;
@@ -64,38 +62,6 @@ describe("AuthService", () => {
     });
   });
 
-  describe("입력한 아이디와 비밀번호가 유저 정보와 일치하는지 확인", () => {
-    let bcryptCompareSpy: jest.SpyInstance;
-
-    beforeEach(() => {
-      bcryptCompareSpy = jest.spyOn(bcrypt, "compare");
-    });
-
-    it("성공", async () => {
-      bcryptCompareSpy.mockReturnValueOnce(true);
-
-      const result = await authService.comparePassword(USER_STUB.password, USER_STUB.password);
-
-      expect(result).toEqual(USER_STUB_NON_PASSWORD);
-    });
-
-    describe("실패", () => {
-      // 실패 조건?
-      // 1. 유저 정보가 없는 경우
-      // 2. 비밀번호가 일치하지 않는 경우
-      it("비밀번호가 일치하지 않는 경우", async () => {
-        bcryptCompareSpy.mockReturnValueOnce(false);
-
-        try {
-          await authService.comparePassword(USER_STUB.password, USER_STUB.password);
-        } catch (e) {
-          expect(e.status).toBe(400);
-          expect(e.message).toBe(USER_ERROR.BAD_REQUEST);
-        }
-      });
-    });
-  });
-
   describe("refresh token 유효성 검사", () => {
     let jwtServiceVerifySpy: jest.SpyInstance;
 
@@ -106,21 +72,20 @@ describe("AuthService", () => {
     it("성공", async () => {
       jwtServiceVerifySpy.mockReturnValueOnce(true);
 
-      const result = await authService.verifyRefreshToken(USER_STUB, USER_STUB.refreshToken);
+      const result = await authService.verifyRefreshToken(USER_STUB.refreshToken);
 
       expect(jwtServiceVerifySpy).toHaveBeenCalledTimes(1);
-      expect(jwtServiceVerifySpy).toHaveBeenCalledWith({
-        _id: USER_STUB_NON_PASSWORD._id,
-      });
+      expect(jwtServiceVerifySpy).toHaveBeenCalledWith(USER_STUB.refreshToken);
       expect(result).toBe(true);
     });
 
     describe("실패", () => {
       it("유저 정보에 refresh token이 없는 경우", async () => {
         try {
-          await authService.verifyRefreshToken(USER_STUB, null);
+          await authService.verifyRefreshToken(null);
         } catch (e) {
           expect(e.status).toBe(401);
+          expect(jwtServiceVerifySpy).toHaveBeenCalledTimes(1);
           expect(e.message).toBe(USER_ERROR.UNAUTHORIZED);
         }
       });
@@ -129,9 +94,10 @@ describe("AuthService", () => {
         jwtServiceVerifySpy.mockRejectedValueOnce(new Error());
 
         try {
-          await authService.verifyRefreshToken(USER_STUB, USER_STUB.refreshToken);
+          await authService.verifyRefreshToken(USER_STUB.refreshToken);
         } catch (e) {
           expect(e.status).toBe(401);
+          expect(jwtServiceVerifySpy).toHaveBeenCalledTimes(1);
           expect(e.message).toBe(USER_ERROR.UNAUTHORIZED);
         }
       });
