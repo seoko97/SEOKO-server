8f7b2afeccaca7db5c6201062ff4987702ed83ce
seokho10007 <sukho1007@naver.com>
2023-07-27 02:17:14 +0900

    feat: User 정보를 가져오기 위한 Decorator 생성

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+
+export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+
+  if (!request.user) {
+    throw new UnauthorizedException("유저 정보가 존재하지 않습니다.");
+  }
+
+  return request.user;
+});
