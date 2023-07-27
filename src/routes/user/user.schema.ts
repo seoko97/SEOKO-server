aa518af1e9000aa593dcbe14ce9e177a5a4901f3
seokho10007 <sukho1007@naver.com>
2023-07-28 01:10:51 +0900

    feat: class-validator 적용

--- src/routes/user/user.schema.ts
+++ src/routes/user/user.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
+import { IsString } from "class-validator";
 import { Document, Model } from "mongoose";
 
 import { BaseSchema } from "@/common/schema/base.schema";
@@ -9,15 +10,19 @@ export type UserModel = Model<UserDocument>;
 
 @Schema()
 export class User extends BaseSchema {
+  @IsString()
   @Prop({ required: true })
   username!: string;
 
+  @IsString()
   @Prop({ required: true })
   userId!: string;
 
+  @IsString()
   @Prop({ required: true })
   password!: string;
 
+  @IsString()
   @Prop({ default: null })
   refreshToken?: string;
 }
