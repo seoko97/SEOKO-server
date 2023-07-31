a2f7414809d8faee5f310265e154160ccaf5bccc
seokho10007 <sukho1007@naver.com>
2023-08-01 00:20:45 +0900

    feat: number id target을 위한 Sequence Module 생성

---
+++ src/common/sequence/sequence.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+
+import { Sequence, SequenceModel } from "@/common/sequence/sequence.schema";
+
+@Injectable()
+export class SequenceRepository {
+  constructor(@InjectModel(Sequence.name) protected readonly sequenceModel: SequenceModel) {}
+
+  async getNextSequence(_id: string) {
+    const sequence = await this.sequenceModel.findOne({ _id });
+
+    if (sequence) {
+      sequence.seq += 1;
+
+      await sequence.save();
+
+      return sequence.seq;
+    } else {
+      const sequenceDocument = await this.sequenceModel.create({ _id });
+
+      return sequenceDocument.seq;
+    }
+  }
+}
