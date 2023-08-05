294c3e56f5ccf4eb2c6b0106f3e7c8ef0df60201
seokho10007 <sukho1007@naver.com>
2023-08-05 21:13:38 +0900

    style: 중복되는 값 상수화

--- test/utils/stub/post.ts
+++ test/utils/stub/post.ts
@@ -1,31 +1,34 @@
 import { SERIES_STUB } from "test/utils/stub/series";
 import { TAG_STUB } from "test/utils/stub/tag";
 
-const POST_CREATE_STUB = {
+const POST_CREATE_STUB_WITHOUT_TAGS_AND_SERIES = {
   title: "title",
   content: "content",
   thumbnail: "thumbnail",
-  series: "series",
+};
+
+const POST_CREATE_STUB = {
+  ...POST_CREATE_STUB_WITHOUT_TAGS_AND_SERIES,
+  series: SERIES_STUB.name,
   tags: ["tags"],
 };
 
 const POST_UPDATE_STUB = {
   _id: "_id",
-  title: "title",
+  ...POST_CREATE_STUB_WITHOUT_TAGS_AND_SERIES,
+  series: "change_series",
   deleteTags: ["delete"],
   addTags: ["add"],
 };
 
 const POST_STUB = {
   _id: "_id",
-  title: "title",
-  content: "content",
-  thumbnail: "thumbnail",
+  ...POST_CREATE_STUB_WITHOUT_TAGS_AND_SERIES,
   series: SERIES_STUB,
-  tags: TAG_STUB,
+  tags: [TAG_STUB],
   likes: ["likes"],
   views: ["views"],
   nid: 1,
 };
 
-export { POST_CREATE_STUB, POST_UPDATE_STUB, POST_STUB };
+export { POST_CREATE_STUB_WITHOUT_TAGS_AND_SERIES, POST_CREATE_STUB, POST_UPDATE_STUB, POST_STUB };
