218e82c5adc2378b0a0e103d1a366d8de09bb678
seokho10007 <sukho1007@naver.com>
2023-08-08 01:05:06 +0900

    feat: post 에러 메시지 추가 (#20)

--- src/utils/constants/post.ts
+++ src/utils/constants/post.ts
@@ -2,6 +2,7 @@ const POST_ERROR = {
   NOT_FOUND: "존재하지 않는 게시글입니다.",
   FAIL_CREATE: "게시글 생성에 실패했습니다.",
   FAIL_UPDATE: "게시글 수정에 실패했습니다.",
+  ALREADY_LIKED: "이미 좋아요를 누른 게시글입니다.",
 };
 
 export { POST_ERROR };
