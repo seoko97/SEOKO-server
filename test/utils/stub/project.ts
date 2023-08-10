ced3896a424d3080c6af11ca812a80d617328cf8
seokho10007 <sukho1007@naver.com>
2023-08-10 19:21:37 +0900

    test: project service 테스트 생성 (#31)

---
+++ test/utils/stub/project.ts
@@ -0,0 +1,23 @@
+import { ProjectDocument } from "@/routes/project/project.schema";
+
+const CREATE_PROJECT_STUB = {
+  title: "title",
+  description: "description",
+  content: "content",
+  thumbnail: "thumbnail",
+  github: "github",
+  start: "2021-01-01",
+  end: "2021-01-01",
+};
+
+const UPDATE_PROJECT_STUB = {
+  _id: "id",
+  ...CREATE_PROJECT_STUB,
+};
+
+const PROJECT_STUB = {
+  ...UPDATE_PROJECT_STUB,
+  nid: 1,
+} as unknown as ProjectDocument;
+
+export { CREATE_PROJECT_STUB, UPDATE_PROJECT_STUB, PROJECT_STUB };
