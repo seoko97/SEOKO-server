74a03bba9e2d0d5e1b2554ac47be0e3001bd0a00
seokho10007 <sukho1007@naver.com>
2023-08-10 21:25:23 +0900

    feat: project controller 비즈니스 로직 구현 (#31)

---
+++ src/routes/project/project.controller.ts
@@ -0,0 +1,48 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+
+import { Public } from "@/common/decorators";
+import { CreateProjectDto } from "@/routes/project/dto/create-project.dto";
+import { UpdateProjectDto } from "@/routes/project/dto/update-project.dto";
+import { ProjectService } from "@/routes/project/project.service";
+
+@Controller("projects")
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Post()
+  async create(@Body() createProjectDto: CreateProjectDto) {
+    const project = await this.projectService.create(createProjectDto);
+
+    return project;
+  }
+
+  @Put(":_id")
+  async update(@Param("_id") _id: string, @Body() updateProjectDto: UpdateProjectDto) {
+    const project = await this.projectService.update(_id, updateProjectDto);
+
+    return project;
+  }
+
+  @Delete(":_id")
+  async delete(@Param("_id") _id: string) {
+    await this.projectService.delete(_id);
+
+    return true;
+  }
+
+  @Public()
+  @Get(":nid")
+  async getByNumId(@Param("nid") nid: number) {
+    const project = await this.projectService.getByNumId(nid);
+
+    return project;
+  }
+
+  @Public()
+  @Get()
+  async getAll() {
+    const projects = await this.projectService.getAll();
+
+    return projects;
+  }
+}
