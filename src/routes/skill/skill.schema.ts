bab636cd1d6554fb06a8461f0eda04bc45ac472c
seokho10007 <sukho1007@naver.com>
2023-08-13 22:00:32 +0900

    feat: skill schema 정의 (#40)

---
+++ src/routes/skill/skill.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+import { IsEnum, IsString } from "class-validator";
+import { Document, Model } from "mongoose";
+
+import { BaseSchema } from "@/common/schema/base.schema";
+import { SkillType } from "@/types";
+
+export type SkillDocument = Skill & Document;
+export type SkillModel = Model<SkillDocument>;
+
+@Schema()
+export class Skill extends BaseSchema {
+  @IsString()
+  @Prop({ required: true, unique: true })
+  name!: string;
+
+  @IsEnum(SkillType)
+  @Prop({ required: true, enum: SkillType })
+  type!: SkillType;
+
+  @IsString()
+  @Prop({ required: true })
+  icon!: string;
+}
+
+export const SkillSchema = SchemaFactory.createForClass(Skill);
