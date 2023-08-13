82aec5c083e1bf3c1531187ce2e1de9a0effe022
seokho10007 <sukho1007@naver.com>
2023-08-13 21:59:04 +0900

    feat: base repository를 상속받는 skill repository 구현 (#40)

---
+++ src/routes/skill/skill.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+
+import { BaseRepository } from "@/common/repository/base.repository";
+import { SequenceRepository } from "@/common/sequence/sequence.repository";
+import { Skill, SkillDocument, SkillModel } from "@/routes/skill/skill.schema";
+import { TFilteredSkills } from "@/types";
+
+@Injectable()
+export class SkillRepository extends BaseRepository<SkillDocument> {
+  constructor(
+    @InjectModel(Skill.name) private readonly skillModel: SkillModel,
+    sequenceRepository: SequenceRepository,
+  ) {
+    super(skillModel, sequenceRepository);
+  }
+
+  async getAllToSkillType() {
+    const skills = await this.skillModel.aggregate([
+      { $group: { _id: "$type", skills: { $push: "$$ROOT" } } },
+    ]);
+
+    return skills.reduce<TFilteredSkills>((acc, { _id, skills }) => {
+      acc[_id] = skills;
+      return acc;
+    }, {} as TFilteredSkills);
+  }
+}
