e2c7dd57f927607ea9bb4604e0da6be51e38b8cf
seokho10007 <sukho1007@naver.com>
2023-08-14 14:47:43 +0900

    feat: Transaction Decorator 생성 (#43)

---
+++ src/common/decorators/transaction.decorator.ts
@@ -0,0 +1,42 @@
+import { InternalServerErrorException } from "@nestjs/common";
+
+import { CONNECTION_NOT_FOUND, TRANSACTION_SESSION } from "@/common/transition/constants";
+import { ALS } from "@/common/transition/core/AsyncLocalStorage";
+import { ConnectionStore } from "@/common/transition/core/ConnectionStore";
+
+function Transactional(): MethodDecorator {
+  return (target: any, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>) => {
+    const originalMethod = descriptor.value;
+    const als = new ALS();
+
+    descriptor.value = async function (...args: any[]) {
+      return als.run(async () => {
+        const connection = new ConnectionStore().getConnection();
+
+        if (!connection) {
+          throw new InternalServerErrorException(CONNECTION_NOT_FOUND);
+        }
+
+        const session = await connection.startSession();
+        als.set(TRANSACTION_SESSION, session);
+
+        try {
+          let result: any;
+
+          await session.withTransaction(async () => {
+            result = await originalMethod.apply(this, args);
+          });
+
+          return result;
+        } catch (error) {
+          throw error;
+        } finally {
+          session.endSession();
+        }
+      });
+    };
+    return descriptor;
+  };
+}
+
+export { Transactional };
