cab8dfbd60c22d8990d0adfa3e4380ac7600442a
seokho10007 <sukho1007@naver.com>
2023-08-14 14:43:28 +0900

    feat: mongoDB 실행시 실행된 Connection을 저장하는 ConnectionStore 추가 (#43)

---
+++ src/common/transition/core/ConnectionStore.ts
@@ -0,0 +1,22 @@
+import { Connection } from "mongoose";
+
+import { TRANSACTION_KEY } from "@/common/transition/constants";
+
+class ConnectionStore {
+  private static instance: ConnectionStore = new ConnectionStore();
+  private _connections: { [K: string]: Connection } = {};
+
+  constructor() {
+    return ConnectionStore.instance;
+  }
+
+  setConnection(connection: Connection, connectionName = TRANSACTION_KEY) {
+    this._connections[connectionName] = connection;
+  }
+
+  getConnection(connectionName = TRANSACTION_KEY) {
+    return this._connections[connectionName];
+  }
+}
+
+export { ConnectionStore };
