f7293a6d4aa2f08756b744582a719142a730519b
seokho10007 <sukho1007@naver.com>
2023-09-16 18:24:47 +0900

    feat: 사용하지 않는 타입 제거

--- src/routes/series/dto/update-series.dto.ts
+++ src/routes/series/dto/update-series.dto.ts
@@ -1,12 +1,5 @@
 import { PartialType } from "@nestjs/mapped-types";
 
-import { IsString } from "class-validator";
-
 import { CreateSeriesDto } from "@/routes/series/dto/create-series.dto";
 
 export class UpdateSeriesDto extends PartialType(CreateSeriesDto) {}
-
-export class UpdateSeriesToPushPostDto {
-  @IsString()
-  postId!: string;
-}
