7aa0715aff857bc441a8fc0771c6c0ad61cf52b7
seokho10007 <sukho1007@naver.com>
2023-12-06 01:24:34 +0900

    feat: x-forwarded-for 헤더를 통해 클라이언트 IP를 가져오는 RealIP 데코레이터 추가

--- src/common/decorators/real-ip.decorator.ts
+++ src/common/decorators/real-ip.decorator.ts
@@ -2,11 +2,10 @@ import { createParamDecorator } from "@nestjs/common";
 
 const RealIp = createParamDecorator((_, ctx) => {
   const req = ctx.switchToHttp().getRequest();
-  const ip =
-    (req.ips.length ? req.ips[0] : req.ip) ||
-    req.headers["x-forwarded-for"] ||
-    req.headers["x-real-ip"] ||
-    "0.0.0.0";
+
+  const forwarded = req.headers["x-forwarded-for"]?.split(", ");
+
+  const ip = forwarded?.[0] || req.ip || req.header("x-real-ip") || "0.0.0.0";
 
   return ip;
 });
