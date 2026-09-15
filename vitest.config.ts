88686c7df3375d9ed10eedb32be51adba6d93d7b
seoko97 <sukho1007@naver.com>
2026-09-15 16:42:07 +0900

    chore : Nest 12 개발 환경 및 검사 도구 전환

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath } from "node:url";
 
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const resolvePath = (path: string) => fileURLToPath(new URL(path, import.meta.url));
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   resolve: {
     alias: {
       "@": resolvePath("./src"),
@@ -12,13 +14,8 @@ export default defineConfig({
     },
   },
   test: {
-    environment: "node",
+    root: "./",
     globals: true,
     include: ["__tests__/**/*.spec.ts"],
-    coverage: {
-      include: ["src/**/*.ts"],
-      provider: "v8",
-      reportsDirectory: "./coverage",
-    },
   },
 });
